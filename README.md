05e7dea654b5663a4a937d934217648b868c9d39
Thomas Bender <thomas.bender82@qmail.cuny.edu>
2024-06-08 22:29:50 -0400

    Implement basic shuffle

---
+++ README.md
@@ -0,0 +1,7 @@
+# Retro Roulette
+
+A C implementation of Buckshot Roulette. With the intention of porting the game to retro devices.
+
+## Game Plan
+
+Develop the core gameplay functionality. Once the gameplay is established, port the game to the Sony Playstation. After it's ported to the playstation we attempt to get graphics and everything else running.
---
+++ main.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+#define LIVE 1
+#define BLANK 0
+#define EMPTY -1
+#define MAX_BULLETS 8
+
+typedef struct
+{
+    int type; /* live or blank */
+} Bullet;
+
+void shuffle(Bullet *array, int quantity)
+{
+    for (int i = quantity - 1; i > 0; i--)
+    {
+        int j = rand() % (i + 1);
+        Bullet temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    printf("Shuffled!\n");
+}
+
+char *printBulletType(Bullet bullet)
+{
+    switch (bullet.type)
+    {
+    case LIVE:
+        return "live";
+    case BLANK:
+        return "blank";
+    case EMPTY:
+        return "empty";
+    default:
+        return "undefined";
+    }
+}
+
+void printBulletArray(Bullet* bulletArray) {
+    for (int i = 0; i < MAX_BULLETS; i++)
+    {
+        if (bulletArray[i].type == EMPTY)
+            break;
+        printf("%s ", printBulletType(bulletArray[i]));
+    }
+    printf("\n");
+}
+
+void generateBulletsArray(Bullet *bulletArray)
+{
+    int i;
+    int quantity = (rand() % (MAX_BULLETS - 1)) + 2;
+    printf("Number of bullets to make: %d\n", quantity);
+    int liveQuantity = quantity / 2;
+
+    if (rand() % 2)
+    {
+        liveQuantity = quantity - liveQuantity;
+    }
+    printf("%d live, %d blank\n", liveQuantity, quantity - liveQuantity);
+
+    for (i = 0; i < quantity; i++)
+    {
+        if (i < liveQuantity)
+            bulletArray[i].type = LIVE;
+        else
+            bulletArray[i].type = BLANK;
+    }
+    for (; i < MAX_BULLETS; i++)
+    {
+        bulletArray[i].type = EMPTY;
+    }
+    shuffle(bulletArray, quantity);
+    printBulletArray(bulletArray);
+}
+
+int main()
+{
+    srand(time(NULL));
+
+    Bullet bulletArray[8];
+    generateBulletsArray(bulletArray);
+
+    return 0;
+}
