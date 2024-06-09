7094039b5a89f17d9fa317425e00c3c17935dbc9
Thomas Bender <thomas.bender82@qmail.cuny.edu>
2024-06-08 23:04:51 -0400

    Get player name.

--- main.c
+++ main.c
@@ -7,11 +7,20 @@
 #define BLANK 0
 #define EMPTY -1
 #define MAX_BULLETS 8
+#define MAX_LIVES 4
 
 typedef struct
 {
     int type; /* live or blank */
 } Bullet;
+
+typedef struct
+{
+    char name[30];
+    int charges;
+} Player;
+
+// Bullet //
 
 void shuffle(Bullet *array, int quantity)
 {
@@ -40,7 +49,8 @@ char *printBulletType(Bullet bullet)
     }
 }
 
-void printBulletArray(Bullet* bulletArray) {
+void printBulletArray(Bullet *bulletArray)
+{
     for (int i = 0; i < MAX_BULLETS; i++)
     {
         if (bulletArray[i].type == EMPTY)
@@ -78,12 +88,39 @@ void generateBulletsArray(Bullet *bulletArray)
     printBulletArray(bulletArray);
 }
 
+// Player //
+void promptPlayerName(Player *player)
+{
+    printf("Enter Name: ");
+    fgets(player->name, sizeof(player->name), stdin);
+    size_t len = strlen(player->name);
+    if (len > 0 && player->name[len - 1] == '\n')
+    {
+        player->name[len - 1] = '\0';
+    }
+}
+
+void assignCharges(Player *player, Player *dealer)
+{
+    int charges = (rand() % (MAX_LIVES - 1)) + 2;
+    player->charges = charges;
+    dealer->charges = charges;
+}
+
 int main()
 {
     srand(time(NULL));
 
     Bullet bulletArray[8];
+    Player dealer;
+    strcpy(dealer.name, "Dealer");
+    Player player;
+    promptPlayerName(&player);
+    assignCharges(&player, &dealer);
+
     generateBulletsArray(bulletArray);
 
+    printf("Dealer Name: %s, Lives: %d\n", dealer.name, dealer.charges);
+    printf("Player Name: %s, Lives: %d\n", player.name, player.charges);
     return 0;
 }
